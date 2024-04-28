99760f91c4beb38b6f4e1fba531b77a7d82a1ebd
rittbpt <nutdanai.t@ku.th>
2024-04-28 07:08:44 +0700

    add prepare db

--- connection/connectdb.go
+++ connection/connectdb.go
@@ -1,17 +1,60 @@
 package connection
 
 import (
-    "database/sql"
-    "log"
-    _ "github.com/lib/pq"
+	"database/sql"
+	"log"
+
+	_ "github.com/lib/pq"
 )
 
 func ConnectDB() (*sql.DB, error) {
-	db, err := sql.Open("postgres", "user=postgres password=postgres dbname=ktaxes sslmode=disable") 
+	db, err := sql.Open("postgres", "user=postgres password=postgres dbname=ktaxes sslmode=disable")
+	if err != nil {
+		return nil, err
+	}
+	log.Println("Database connection successful")
+	return db, nil
+}
 
-    if err != nil {
-        return nil, err
-    }
-    log.Println("Database connection successful")
-    return db, nil
+func PrepareDB() {
+	db, err := ConnectDB()
+	if err != nil {
+		log.Fatal("Error connecting to the database:", err)
+	}
+
+	rows, err := db.Query("SELECT EXISTS (SELECT FROM information_schema.tables WHERE table_name = 'tax')")
+	if err != nil {
+		log.Fatal("Error checking if 'tax' table exists:", err)
+	}
+	defer rows.Close()
+
+	var exists bool
+	for rows.Next() {
+		if err := rows.Scan(&exists); err != nil {
+			log.Fatal("Error scanning rows:", err)
+		}
+	}
+
+	if !exists {
+		if _, err := db.Exec(`CREATE TABLE tax (
+			id SERIAL PRIMARY KEY,
+			k_reciept_deduct INTEGER,
+			personal_deduct INTEGER,
+			active BOOLEAN,
+			create_time TIMESTAMP
+		)`); err != nil {
+			log.Fatal("Error creating 'tax' table:", err)
+		}
+		log.Println("Table 'tax' created successfully")
+
+		if _, err := db.Exec(`INSERT INTO tax (k_reciept_deduct, personal_deduct, active, create_time)
+			VALUES (50000, 60000, true, NOW())`); err != nil {
+			log.Fatal("Error inserting initial data into 'tax' table:", err)
+		}
+		log.Println("Initial data inserted into 'tax' table successfully")
+	} else {
+		log.Println("Table 'tax' already exists")
+	}
+
+	defer db.Close()
 }
--- main.go
+++ main.go
@@ -8,27 +8,34 @@ import (
 	"github.com/rittbpt/assessment-tax/service"
 	"github.com/rittbpt/assessment-tax/connection"
 	"github.com/rittbpt/assessment-tax/route"
-
 )
 
 func main() {
+	// เตรียมฐานข้อมูล
+	connection.PrepareDB()
+
+	// สร้าง instance ของ Echo
 	e := echo.New()
 
+	// Middleware
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
 
+	// เชื่อมต่อฐานข้อมูล
 	db, err := connection.ConnectDB()
-    if err != nil {
-        e.Logger.Fatal("Failed to connect to database:", err)
-    }
+	if err != nil {
+		e.Logger.Fatal("Failed to connect to database:", err)
+	}
+	connection.PrepareDB()
+
+	// สร้าง repository, service และ controller
 	taxRepository := repository.NewTaxRepository(db)
-
 	taxService := service.NewTaxService(taxRepository)
-
 	taxController := controller.NewTaxController(taxService)
 
+	// กำหนดเส้นทางของ API
 	route.TaxRoutes(e, taxController)
 
+	// เริ่มต้นเซิร์ฟเวอร์
 	e.Logger.Fatal(e.Start(":8080"))
-
 }
